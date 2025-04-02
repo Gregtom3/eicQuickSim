c2f17edfb28ae9a8069593bde394808b43fa8a19
Gregtom3 <gamatousek@gmail.com>
2025-04-02 15:12:51 -0400

    ?

--- src/eicQuickSim/Weights.C
+++ src/eicQuickSim/Weights.C
@@ -291,6 +291,7 @@ void Weights::loadPrecalculatedWeights(const std::string &precalcCSVFilename) {
 ///////////////////////////////////////////////////////////
 double Weights::getWeight(double Q2) const {
     int idx = -1;
+    std::cout << "A" << std::endl;
     for (size_t i = 0; i < Q2mins.size(); i++) {
         if (inQ2Range(Q2, Q2mins[i], Q2maxs[i], false)) {
             std::cout << "HERE" << std::endl;
